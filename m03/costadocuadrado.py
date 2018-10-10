b5c6aab98ff7d6394885c5b75d4ba999e73a26a7
Alejandro <alejandrozeus21@gmail.com>
2018-10-10 08:23:33 +0200

    Create costadocuadrado.py

---
+++ m03/costadocuadrado.py
@@ -0,0 +1,21 @@
+# !/usr/bin/python
+# -*-coding: utf-8-*-
+
+#Alejandro López	
+#isx43567395	
+#04/10/2018	
+#Version 1
+
+#Que hace el script --> Programa que lee un costado cuadrado
+#Especificaciones de Entrada
+# Nombre entero del costado.
+
+numero=int(input('¿Que costado cuadrado quieres calcular? '))
+
+area= numero**2  
+
+perimetro= numero * 4
+
+print('El resultado del area es ', area)
+
+print ('El resultado del perimetro es ', perimetro)
